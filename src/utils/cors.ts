d23c1ccf80511c21f12c19cc1c42e5cd1e0f16f3
sadabdhrubo1997 <sadabdhrubo@gmail.com>
2022-08-27 02:59:27 +0600

    new frontend url added

--- src/utils/cors.ts
+++ src/utils/cors.ts
@@ -1,5 +1,9 @@
 export const corsOptions = {
-  origin: ['http://localhost:3000', 'https://localhost:3002', ],
+  origin: [
+    'http://localhost:3000',
+    'https://localhost:3002',
+    'https://sadabdhrubo-fullstack-todo-app.netlify.app',
+  ],
   // methods: '*',
   credentials: true,
 };
